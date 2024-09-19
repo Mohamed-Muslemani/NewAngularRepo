e2b9ff833a16c6d1f8483d53f58a65a046ebd283
Mohamed-Muslemani <w0865765@myscc.ca>
2024-09-19 17:06:22 -0400

    Finished Lab3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,18 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {User} from "./Shared/Models/user";
 import {JsonPipe, NgForOf} from "@angular/common";
+import {StudentListComponent} from "./student-list/student-list.component";
+import {StudentDetailComponent} from "./student-detail/student-detail.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, JsonPipe],
+  imports: [RouterOutlet, NgForOf, JsonPipe, StudentListComponent, StudentDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title= 'User Generation';
-
-  user1 : User = {id: 1, firstName: "Mohamed", lastName: "Muslemani", department: "Programming", isAdmin: false};
-  user2 : User = {id: 2, firstName: "Darren", lastName: "Takakki", department: "Web Dev", isAdmin: true};
-  //Can declare values either way
-  userList: User[] = //any[] would have worked as well
-    [this.user1,this.user2,
-      {id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
-      {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin:true}
-    ]
-//Function that gets called from our onclick. Takes in an
-  //arguement ofa variable called user, which is type User and returns void
-  toggleAdminStatus(user: User): void {
-    user.isAdmin = !user.isAdmin;
-  }
-
-
-
-
-
 }
