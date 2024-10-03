871d06c3d072f710209331d950af0946a5bae319
Mohamed-Muslemani <w0865765@myscc.ca>
2024-10-03 16:37:07 -0400

    Finish Lab4

--- src/app/Services/student.service.ts
+++ src/app/Services/student.service.ts
@@ -14,8 +14,29 @@ export class StudentService {
     return of(userList);
   }
 
-  addStudent(newStudent:User) : Observable<User[]>{
-    this.students.push(newStudent)
-    return of(this.students);
+  getStudentById(id: number): Observable<User | undefined> {
+    return of(this.students.find(student => student.id === id));
+  }
+
+  addStudent(student: User): Observable<User> {
+    this.students.push(student);
+    return of(student);
+  }
+
+  updateStudent(updatedStudent: User): Observable<User | undefined> {
+    const index = this.students.findIndex(student => student.id === updatedStudent.id);
+    if (index > -1) {
+      this.students[index] = updatedStudent;
+      return of(updatedStudent);
+    }
+    return of(undefined);
+  }
+
+  deleteStudent(id: number): void {
+    this.students = this.students.filter(student => student.id !== id);
+  }
+  // New method to generate a new unique ID
+  generateNewId(): number {
+    return this.students.length > 0 ? Math.max(...this.students.map(student => student.id)) + 1 : 1;
   }
 }
